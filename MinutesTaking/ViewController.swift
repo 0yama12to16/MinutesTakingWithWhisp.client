27b692e64cb83365075796d424ec41c16b725bac
toya oyama <touya121216@gmail.com>
2022-10-25 18:29:47 +0900

    Initial Commit

---
+++ MinutesTaking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MinutesTaking
+//
+//  Created by OyamaToya on 2022/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
